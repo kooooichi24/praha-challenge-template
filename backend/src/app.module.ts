dc786de01a4de402f8046e40f8ca240a6bc190a7
古川広一 <ex.kouichi1224@gmail.com>
2021-07-20 00:16:06 +0900

    fix: some-data の削除漏れ

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common'
-import { SampleController } from './controller/sample/some-data.controller'
 import { UserController } from './controller/user/user.controller'
 
 // memo: DIコンテナとしては使わないため、controllerの追加だけしてください
 @Module({
   imports: [],
-  controllers: [SampleController, UserController],
+  controllers: [UserController],
   providers: [],
 })
 export class AppModule {}
