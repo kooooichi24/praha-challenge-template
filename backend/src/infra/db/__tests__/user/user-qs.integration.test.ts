b82b72eef86c5e315891c3d1e2ce30660e82b74b
古川広一 <ex.kouichi1224@gmail.com>
2021-11-06 23:09:08 +0900

    user-qs のITのリファクタ

--- backend/src/infra/db/__tests__/user/user-qs.integration.test.ts
+++ backend/src/infra/db/__tests__/user/user-qs.integration.test.ts
@@ -1,60 +1,220 @@
 import { prisma } from '@testUtil/prisma'
 import { UserQS } from '../../query-service/user/user-qs'
 import { UserDTO } from 'src/app/user/query-service-interface/user-qs'
-import { createRandomIdString } from 'src/util/random'
+import { Page } from 'src/app/shared/Paging'
 
 describe('user-qs.integration.ts', () => {
   const userQS = new UserQS(prisma)
 
-  beforeAll(async () => {
-    await prisma.users.deleteMany({})
+  beforeEach(async () => {
+    await setBaseDb()
   })
+
+  afterEach(async () => {
+    await prisma.users.deleteMany({})
+    await prisma.tasks.deleteMany({})
+    await prisma.userTaskStatus.deleteMany({})
+  })
+
   afterAll(async () => {
     await prisma.$disconnect()
   })
 
   describe('getAll', () => {
-    afterEach(async () => {
-      await prisma.users.deleteMany({})
-    })
-
     it('[正常系]userを取得できる', async () => {
       // Arrange
       const usersExpected = [
         new UserDTO({
-          id: '1',
-          name: 'furukawa',
-          mail: 'furukawa@gmai.com',
+          id: 'user1',
+          name: 'user1',
+          mail: 'user1@example.com',
           status: 'ENROLLMENT',
-          tasksStatus: [],
+          tasksStatus: [
+            {
+              userId: 'user1',
+              taskId: 'task1',
+              status: 'DONE',
+            },
+            {
+              userId: 'user1',
+              taskId: 'task2',
+              status: 'REVIEWING',
+            },
+            {
+              userId: 'user1',
+              taskId: 'task3',
+              status: 'TODO',
+            },
+            {
+              userId: 'user1',
+              taskId: 'task4',
+              status: 'TODO',
+            },
+            {
+              userId: 'user1',
+              taskId: 'task5',
+              status: 'TODO',
+            },
+            {
+              userId: 'user1',
+              taskId: 'task6',
+              status: 'TODO',
+            },
+          ],
         }),
         new UserDTO({
-          id: '2',
-          name: 'nakano',
-          mail: 'nakano@gmai.com',
+          id: 'user2',
+          name: 'user2',
+          mail: 'user2@example.com',
           status: 'ENROLLMENT',
-          tasksStatus: [],
+          tasksStatus: [
+            {
+              userId: 'user2',
+              taskId: 'task1',
+              status: 'DONE',
+            },
+            {
+              userId: 'user2',
+              taskId: 'task2',
+              status: 'DONE',
+            },
+            {
+              userId: 'user2',
+              taskId: 'task3',
+              status: 'REVIEWING',
+            },
+            {
+              userId: 'user2',
+              taskId: 'task4',
+              status: 'TODO',
+            },
+            {
+              userId: 'user2',
+              taskId: 'task5',
+              status: 'TODO',
+            },
+            {
+              userId: 'user2',
+              taskId: 'task6',
+              status: 'TODO',
+            },
+          ],
         }),
         new UserDTO({
-          id: '3',
-          name: 'sasaki',
-          mail: 'sasaki@gmai.com',
+          id: 'user3',
+          name: 'user3',
+          mail: 'user3@example.com',
           status: 'ENROLLMENT',
-          tasksStatus: [],
+          tasksStatus: [
+            {
+              userId: 'user3',
+              taskId: 'task1',
+              status: 'DONE',
+            },
+            {
+              userId: 'user3',
+              taskId: 'task2',
+              status: 'DONE',
+            },
+            {
+              userId: 'user3',
+              taskId: 'task3',
+              status: 'DONE',
+            },
+            {
+              userId: 'user3',
+              taskId: 'task4',
+              status: 'REVIEWING',
+            },
+            {
+              userId: 'user3',
+              taskId: 'task5',
+              status: 'TODO',
+            },
+            {
+              userId: 'user3',
+              taskId: 'task6',
+              status: 'TODO',
+            },
+          ],
+        }),
+        new UserDTO({
+          id: 'user4',
+          name: 'user4',
+          mail: 'user4@example.com',
+          status: 'RECESS',
+          tasksStatus: [
+            {
+              userId: 'user4',
+              taskId: 'task1',
+              status: 'DONE',
+            },
+            {
+              userId: 'user4',
+              taskId: 'task2',
+              status: 'DONE',
+            },
+            {
+              userId: 'user4',
+              taskId: 'task3',
+              status: 'DONE',
+            },
+            {
+              userId: 'user4',
+              taskId: 'task4',
+              status: 'DONE',
+            },
+            {
+              userId: 'user4',
+              taskId: 'task5',
+              status: 'REVIEWING',
+            },
+            {
+              userId: 'user4',
+              taskId: 'task6',
+              status: 'TODO',
+            },
+          ],
+        }),
+        new UserDTO({
+          id: 'user5',
+          name: 'user5',
+          mail: 'user5@example.com',
+          status: 'LEFT',
+          tasksStatus: [
+            {
+              userId: 'user5',
+              taskId: 'task1',
+              status: 'DONE',
+            },
+            {
+              userId: 'user5',
+              taskId: 'task2',
+              status: 'DONE',
+            },
+            {
+              userId: 'user5',
+              taskId: 'task3',
+              status: 'DONE',
+            },
+            {
+              userId: 'user5',
+              taskId: 'task4',
+              status: 'DONE',
+            },
+            {
+              userId: 'user5',
+              taskId: 'task5',
+              status: 'DONE',
+            },
+            {
+              userId: 'user5',
+              taskId: 'task6',
+              status: 'REVIEWING',
+            },
+          ],
         }),
       ]
-      await Promise.all(
-        usersExpected.map(async (user: UserDTO) => {
-          await prisma.users.create({
-            data: {
-              id: user.id,
-              name: user.name,
-              mail: user.mail,
-              status: user.status,
-            },
-          })
-        }),
-      )
 
       // Act
       const actual = await userQS.findAll()
@@ -65,31 +225,49 @@ describe('user-qs.integration.ts', () => {
   })
 
   describe('findById', () => {
-    afterEach(async () => {
-      await prisma.users.deleteMany({})
-    })
-
     it('[正常系]idに合致したuserを取得できる', async () => {
       // Arrange
-      const id = createRandomIdString()
       const expected = new UserDTO({
-        id,
-        name: 'name',
-        mail: 'mail@gmail.com',
+        id: 'user1',
+        name: 'user1',
+        mail: 'user1@example.com',
         status: 'ENROLLMENT',
-        tasksStatus: [],
-      })
-      await prisma.users.create({
-        data: {
-          id,
-          name: 'name',
-          mail: 'mail@gmail.com',
-          status: 'ENROLLMENT',
-        },
+        tasksStatus: [
+          {
+            userId: 'user1',
+            taskId: 'task1',
+            status: 'DONE',
+          },
+          {
+            userId: 'user1',
+            taskId: 'task2',
+            status: 'REVIEWING',
+          },
+          {
+            userId: 'user1',
+            taskId: 'task3',
+            status: 'TODO',
+          },
+          {
+            userId: 'user1',
+            taskId: 'task4',
+            status: 'TODO',
+          },
+          {
+            userId: 'user1',
+            taskId: 'task5',
+            status: 'TODO',
+          },
+          {
+            userId: 'user1',
+            taskId: 'task6',
+            status: 'TODO',
+          },
+        ],
       })
 
       // Act
-      const actual = await userQS.findById(id)
+      const actual = await userQS.findById('user1')
 
       // Assert
       expect(actual).toStrictEqual(expected)
@@ -104,4 +282,271 @@ describe('user-qs.integration.ts', () => {
       expect(actual).toBeUndefined()
     })
   })
+
+  // describe('fetchPageByTaskAndStatus', () => {
+  //   test('正常系', async () => {
+  //     // Arrange
+  //     const expectedPage = new Page(
+  //       [
+  //         new UserDTO({
+  //           id: '1',
+  //           name: 'furukawa',
+  //           mail: 'furukawa@gmai.com',
+  //           status: 'ENROLLMENT',
+  //           tasksStatus: [],
+  //         }),
+  //         new UserDTO({
+  //           id: '2',
+  //           name: 'nakano',
+  //           mail: 'nakano@gmai.com',
+  //           status: 'ENROLLMENT',
+  //           tasksStatus: [],
+  //         }),
+  //         new UserDTO({
+  //           id: '3',
+  //           name: 'sasaki',
+  //           mail: 'sasaki@gmai.com',
+  //           status: 'ENROLLMENT',
+  //           tasksStatus: [],
+  //         }),
+  //       ],
+  //       {
+  //         totalCount: 0,
+  //         pageSize: 0,
+  //         pageNumber: 0,
+  //       },
+  //     )
+
+  //     // Act
+  //     const actual = await userQS.findAll()
+
+  //     // Assert
+  //     expect(actual).toEqual(expectedPage)
+  //   })
+  // })
 })
+
+async function setBaseDb(): Promise<void> {
+  await prisma.users.createMany({
+    data: [
+      {
+        id: 'user1',
+        name: 'user1',
+        mail: 'user1@example.com',
+        status: 'ENROLLMENT',
+      },
+      {
+        id: 'user2',
+        name: 'user2',
+        mail: 'user2@example.com',
+        status: 'ENROLLMENT',
+      },
+      {
+        id: 'user3',
+        name: 'user3',
+        mail: 'user3@example.com',
+        status: 'ENROLLMENT',
+      },
+      {
+        id: 'user4',
+        name: 'user4',
+        mail: 'user4@example.com',
+        status: 'RECESS',
+      },
+      {
+        id: 'user5',
+        name: 'user5',
+        mail: 'user5@example.com',
+        status: 'LEFT',
+      },
+    ],
+  })
+  await prisma.tasks.createMany({
+    data: [
+      {
+        id: 'task1',
+        title: 'title1',
+        content: 'content1',
+      },
+      {
+        id: 'task2',
+        title: 'title2',
+        content: 'content2',
+      },
+      {
+        id: 'task3',
+        title: 'title3',
+        content: 'content3',
+      },
+      {
+        id: 'task4',
+        title: 'title4',
+        content: 'content4',
+      },
+      {
+        id: 'task5',
+        title: 'title5',
+        content: 'content5',
+      },
+      {
+        id: 'task6',
+        title: 'title6',
+        content: 'content6',
+      },
+    ],
+  })
+  await prisma.userTaskStatus.createMany({
+    data: [
+      {
+        userId: 'user1',
+        taskId: 'task1',
+        status: 'DONE',
+      },
+      {
+        userId: 'user1',
+        taskId: 'task2',
+        status: 'REVIEWING',
+      },
+      {
+        userId: 'user1',
+        taskId: 'task3',
+        status: 'TODO',
+      },
+      {
+        userId: 'user1',
+        taskId: 'task4',
+        status: 'TODO',
+      },
+      {
+        userId: 'user1',
+        taskId: 'task5',
+        status: 'TODO',
+      },
+      {
+        userId: 'user1',
+        taskId: 'task6',
+        status: 'TODO',
+      },
+      {
+        userId: 'user2',
+        taskId: 'task1',
+        status: 'DONE',
+      },
+      {
+        userId: 'user2',
+        taskId: 'task2',
+        status: 'DONE',
+      },
+      {
+        userId: 'user2',
+        taskId: 'task3',
+        status: 'REVIEWING',
+      },
+      {
+        userId: 'user2',
+        taskId: 'task4',
+        status: 'TODO',
+      },
+      {
+        userId: 'user2',
+        taskId: 'task5',
+        status: 'TODO',
+      },
+      {
+        userId: 'user2',
+        taskId: 'task6',
+        status: 'TODO',
+      },
+      {
+        userId: 'user3',
+        taskId: 'task1',
+        status: 'DONE',
+      },
+      {
+        userId: 'user3',
+        taskId: 'task2',
+        status: 'DONE',
+      },
+      {
+        userId: 'user3',
+        taskId: 'task3',
+        status: 'DONE',
+      },
+      {
+        userId: 'user3',
+        taskId: 'task4',
+        status: 'REVIEWING',
+      },
+      {
+        userId: 'user3',
+        taskId: 'task5',
+        status: 'TODO',
+      },
+      {
+        userId: 'user3',
+        taskId: 'task6',
+        status: 'TODO',
+      },
+      {
+        userId: 'user4',
+        taskId: 'task1',
+        status: 'DONE',
+      },
+      {
+        userId: 'user4',
+        taskId: 'task2',
+        status: 'DONE',
+      },
+      {
+        userId: 'user4',
+        taskId: 'task3',
+        status: 'DONE',
+      },
+      {
+        userId: 'user4',
+        taskId: 'task4',
+        status: 'DONE',
+      },
+      {
+        userId: 'user4',
+        taskId: 'task5',
+        status: 'REVIEWING',
+      },
+      {
+        userId: 'user4',
+        taskId: 'task6',
+        status: 'TODO',
+      },
+      {
+        userId: 'user5',
+        taskId: 'task1',
+        status: 'DONE',
+      },
+      {
+        userId: 'user5',
+        taskId: 'task2',
+        status: 'DONE',
+      },
+      {
+        userId: 'user5',
+        taskId: 'task3',
+        status: 'DONE',
+      },
+      {
+        userId: 'user5',
+        taskId: 'task4',
+        status: 'DONE',
+      },
+      {
+        userId: 'user5',
+        taskId: 'task5',
+        status: 'DONE',
+      },
+      {
+        userId: 'user5',
+        taskId: 'task6',
+        status: 'REVIEWING',
+      },
+    ],
+  })
+}
